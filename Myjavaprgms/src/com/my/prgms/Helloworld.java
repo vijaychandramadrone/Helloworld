a1051b9fdecb31bc5c58ca9c46a448d604b23eb3
Vijay Chandra <vijay.c@madronesoft.com>
2012-08-16 12:53:20 +0530

    Second Commit

---
+++ Myjavaprgms/src/com/my/prgms/Helloworld.java
@@ -0,0 +1,14 @@
+package com.my.prgms;
+
+public class Helloworld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
