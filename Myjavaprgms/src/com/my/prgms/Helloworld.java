d61f8f4805c2523c82a0b325e58cafd2efdd2be4
Vijay Chandra <vijay.c@madronesoft.com>
2012-08-16 13:04:51 +0530

    Third commit

--- Myjavaprgms/src/com/my/prgms/Helloworld.java
+++ Myjavaprgms/src/com/my/prgms/Helloworld.java
@@ -5,9 +5,13 @@ public class Helloworld {
 	/**
 	 * @param args
 	 */
+	 static int a;
+	 static int b;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello world"+(a+b));
+		System.out.println("Hello this is second line go be pushed");
+		
 
 	}
 
