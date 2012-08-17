fe8dbe0ebdde27f88b0048c17a1efed892a18bba
vijaychandramadrone <vijay.c@madronesoft.com>
2012-08-17 11:45:33 +0530

    Update Myjavaprgms/src/com/my/prgms/Helloworld.java

--- Myjavaprgms/src/com/my/prgms/Helloworld.java
+++ Myjavaprgms/src/com/my/prgms/Helloworld.java
@@ -9,9 +9,10 @@ public class Helloworld {
 	 static int b;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world"+(a+b));
-		System.out.println("Hello this is second line go be pushed");
-		System.out.println("Hello this is second line go be pushed");
+                a=90;
+                b=34;
+		System.out.println("hello world value of added value is"+(a+b));
+
 		
 
 	}
