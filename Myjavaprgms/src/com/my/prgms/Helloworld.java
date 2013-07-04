cd5ce2fa019197e779b543661954a1f32980e092
Vijay Chandra <vijay.c@madronesoft.com>
2013-07-04 11:49:31 +0530

    My checkin to my repository

--- Myjavaprgms/src/com/my/prgms/Helloworld.java
+++ Myjavaprgms/src/com/my/prgms/Helloworld.java
@@ -1,23 +1,15 @@
 package com.my.prgms;
 
 public class Helloworld {
-
-	/**
-	 * @param args
-	 */
 	 static int a;
 	 static int b;
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
         a=90;
         b=34;
             System.out.println("hello world value of added value is"+(a+b));
             System.out.println("value after multiplication is "+(a*b));
-	    System.out.println("value after division is "+(a/b));
-	    System.out.println("value after division is "+(a-b));
-	    /*This class is doing basic maths operation */
-
+	        System.out.println("value after division is "+(a/b));
+	        System.out.println("value after division is "+(a-b));
 	}
 
 }
