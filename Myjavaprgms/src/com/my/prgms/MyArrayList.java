cf1427f3f19f496ed88db0c0838f5d920945f0d0
Vijay Chandra <vijay.c@madronesoft.com>
2013-07-04 12:08:23 +0530

    My Commit to my repo stage

---
+++ Myjavaprgms/src/com/my/prgms/MyArrayList.java
@@ -0,0 +1,13 @@
+package com.my.prgms;
+import java.util.*;
+public class MyArrayList {
+	public static void main(String args[]){
+		ArrayList<String> ar = new ArrayList<String>();
+		ar.add("vijay");
+		ar.add("chandra");
+		System.out.println("First Element in ArrayList "+ ar.get(0));
+		System.out.println("Second Element in ArrayList "+ ar.get(1));
+	}
+	
+
+}
